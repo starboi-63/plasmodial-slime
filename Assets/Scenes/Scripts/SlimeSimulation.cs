90f1788746691ccf51b8214aa34d558499d1f346
Tanish Makadia <tanishmakadia@gmail.com>
2023-12-01 22:56:25 -0500

    chore: Add OnDestroy function to SlimeSimulation

--- Assets/Scenes/Scripts/SlimeSimulation.cs
+++ Assets/Scenes/Scripts/SlimeSimulation.cs
@@ -7,12 +7,18 @@ public class SlimeSimulation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Called when the attached Object is destroyed.
+    void OnDestroy()
+    {
+
     }
 }
